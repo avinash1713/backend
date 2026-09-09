2821caa3e6049473cf08272896660b63748d803b
avinash1713 <ay099472@gmail.com>
2026-09-09 18:09:43 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudnary Url
+      requied: true,
+    },
+    thumbnail: {
+      type: String, //cloudnary Url
+      requied: true,
+    },
+    title: {
+      type: String,
+      requied: true,
+    },
+    description: {
+      type: String,
+      requied: true,
+    },
+    duration: {
+      type: Number,
+      requied: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "Üser",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,7 +4,7 @@ const asyncHandler = (requestHandler) => {
   };
 };
 
-export { asyncHanlder };
+export { asyncHandler };
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 //   try {
