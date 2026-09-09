74fe0d10d825ff83833c1f465eb9f95ddf908b00
avinash1713 <ay099472@gmail.com>
2026-09-09 10:20:57 +0530

    uitls code discussion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./.env" });
-connectDB();
+
+connectDB() //this will return a promise as we are using a sync await syntax
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed");
+  });
 
 /*
 import mongoose from "mongoose";
