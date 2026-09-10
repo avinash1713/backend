fbcec21fe83a81469b4aa30dbc5c940e6f56e442
avinash1713 <ay099472@gmail.com>
2026-09-10 12:08:51 +0530

    add route and controllers setup

--- src/app.js
+++ src/app.js
@@ -13,5 +13,12 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+//router import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declration
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
